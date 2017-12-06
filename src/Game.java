c443d07d1bba11cd1ce4f271d17c442eefe5f0c3
Griffin Clark <32806921+GriffClark@users.noreply.github.com>
2017-12-06 12:23:05 -0800

    building turn system

--- src/Game.java
+++ src/Game.java
@@ -46,33 +46,33 @@ public class Game {
 		players[0] = player1;
 		players[1] = player2;
 		
-		Ship[][] grid = new Ship[15][7]; //this will hold the location of all the ships
+		Ship[][] grid = new Ship[7][15]; //this will hold the location of all the ships
 		
 		//initializing ships. 2 players one imperial one rebel rebel gets to choose all ships imperial gets simpler choices
 		StarDestroyer destroyer1 = new StarDestroyer();
 		StarDestroyer destroyer2 = new StarDestroyer();
 		DeathStar deathStar = new DeathStar();
 
-		grid[0][3] = destroyer1;
-		grid[0][5] = destroyer2;
-		grid[0][4] = deathStar;
-		for (int i = 0; i < grid[0].length; i++)
+		grid[2][0] = destroyer1;
+		grid[4][0] = destroyer2;
+		grid[3][0] = deathStar;
+		for (int i = 0; i < grid.length; i++)
 		{
 			TIEFighter tiefighter = new TIEFighter();
-			grid[1][i] = tiefighter;
+			grid[i][1] = tiefighter;
 		} //should give 7 tiefighters
 		
-		for (int i = 3; i < 6; i++) //should generate 3 X-Wings
+		for (int i = 2; i < 5; i++) //should generate 3 X-Wings
 		{
 			XWing x = new XWing();
-			grid[14][i] = x;
+			grid[i][13] = x;
 		}
 		
 		BWing b1 = new BWing();
 		BWing b2 = new BWing();
 		
-		grid[15][0] = b1;
-		grid[15][7] = b2;
+		grid[0][14] = b1;
+		grid[6][14] = b2;
 		
 		//let's see what this looks like
 		for(int i = 0; i < grid.length; i++)
@@ -85,12 +85,58 @@ public class Game {
 				}
 				else
 				{
-					System.out.print(grid[i][j].getGraphic); //fix me
+					System.out.print(grid[i][j].getGraphic() + " "); 
 				}
 			}
+			System.out.println();
 		}
-		
-		
+		//the game loop
+		int turnCounter = 0;
+		while(true)
+		{
+			if (turnCounter > 0 && turnCounter%2 == 1)
+			{
+				player1.takeTurn();
+				player2.passTurn();
+				System.out.println("Player 1 turn");
+			}
+			
+			else
+			{
+				player1.passTurn();
+				player2.takeTurn();
+				System.out.println("Player 2 turn");
+			}
+			turnCounter ++;
+			
+			System.out.println ("Game is running. " + turnCounter + " turns taken");
+			String input = s.nextLine();
+			if (input.equals("get info"))
+			{
+				System.out.println("please enter x coordinate 1-7");
+				int xCord = s.nextInt();
+				System.out.println("please enter y coordinate 1-15");
+				int yCord = s.nextInt();
+				if(xCord > 7 || yCord > 15)
+				{
+					System.out.println("unhandled out of bounds error. Please do other things");
+				}
+				else
+				{
+					if (grid[xCord - 1][yCord - 1] != null)
+					{
+						System.out.println(grid[xCord - 1][yCord - 1].toString());
+					}
+					else
+					{
+						System.out.println("spot is empty");
+					}
+				}
+			}
+			
+			//how to end turn
+			
+		}
 		
 	}
 
